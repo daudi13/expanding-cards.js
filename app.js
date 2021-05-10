234f9541ba807d8f2d910c7824a4cf82d0d455e5
daudi13 <davouma13@gmail.com>
2021-05-10 09:45:18 +0300

    commit update

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const myCards = document.querySelectorAll('.card');
 
 myCards.forEach((card) => {
     card.addEventListener('click', () => {
-        removeActiveClasses()
+        removeActiveClass();
         card.classList.add('active');
     })
 })
 
-function removeActiveClasses() {
-        myCards.forEach(card => {
-        card.classList.remove('active');
+function removeActiveClass() {
+    myCards.forEach((card) => {
+        card.classList.remove('active')
     })
 }
-
